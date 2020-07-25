87b765140e0f78a97dbbd7fd43ec57ab43f34209
ronaldo.gomes <ronaldo.gomes@cgi.com>
2020-07-25 16:31:25 -0400

    adding ssl support

--- app.js
+++ app.js
@@ -14,15 +14,32 @@ const argv = yargs
     alias: "p",
     type: "number",
   })
+  .option("sslport", {
+    default: 5443,
+    type: "number",
+  })
+  .option("ssl", {
+    default: false,
+    type: "boolean",
+  })
   .help()
   .alias("help", "h").argv;
 
-const proxy = require("redbird")({ port: argv.port });
+const proxy = require("redbird")({
+  port: argv.port,
+  ssl: {
+    port: argv.sslport,
+    key: "certs/dev-key.pem",
+    cert: "certs/dev-cert.pem",
+  },
+});
 
 argv.map.forEach((element) => {
   const source = element.split(",")[0].trim();
   const target = element.split(",")[1].trim();
-  proxy.register(source, target);
+  proxy.register(source, target, {
+    ssl: argv.ssl,
+  });
 });
 
 if (argv.verbose) {
