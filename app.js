c94b22f3c3539407151229ef03a2602a6efeca50
ronaldo.gomes <ronaldo.gomes@cgi.com>
2020-07-26 11:08:04 -0400

    version 0.0.2

--- app.js
+++ app.js
@@ -1,30 +1,4 @@
-const yargs = require("yargs");
-
-const argv = yargs
-  .usage("Usage: $0 -p [port] -map 'source, target'")
-  .array("map")
-  .default("map", [], "empty list")
-  .option("verbose", {
-    alias: "v",
-    type: "boolean",
-    description: "Run with verbose logging",
-  })
-  .option("port", {
-    default: 5000,
-    alias: "p",
-    type: "number",
-  })
-  .option("sslport", {
-    default: 5443,
-    type: "number",
-  })
-  .option("ssl", {
-    default: false,
-    type: "boolean",
-  })
-  .help()
-  .alias("help", "h").argv;
-
+const { argv } = require("./command-line.js");
 const proxy = require("redbird")({
   port: argv.port,
   ssl: {
