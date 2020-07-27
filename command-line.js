ff9a2d0915e1299f1c803f14851dbea672638e00
ronaldo.gomes <ronaldo.gomes@cgi.com>
2020-07-27 12:04:18 -0400

    supporting a file as input for host entries

--- command-line.js
+++ command-line.js
@@ -1,7 +1,7 @@
 const yargs = require("yargs");
 
 const argv = yargs
-  .usage("Usage: $0 -p [port] -map 'source, target'")
+  .usage("Usage: $0 -p [port] --map 'source, target'")
   .array("map")
   .default("map", [], "empty list")
   .option("verbose", {
@@ -21,6 +21,10 @@ const argv = yargs
   .option("ssl", {
     default: false,
     type: "boolean",
+  })
+  .option("file", {
+    alias: "f",
+    type: "string",
   })
   .help()
   .alias("help", "h").argv;
